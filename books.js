b661cc189c19ac021445d51171d4f021da1b3d44
giryna13 <zhipster@gmail.com>
2016-09-11 18:36:34 +0300

    added some files

---
+++ books.js
@@ -0,0 +1,21 @@
+var a = [
+	{
+		"name": "Bookend",
+		"author": [
+			"Jim Kery", 
+			"Bob Bobby"
+		],
+		"year": 1986,
+		"readable": true
+	},
+
+	{
+		"name": "Land",
+		"author": [
+			"Bry Bry",
+			"John Film"
+		], 
+		"year": null,
+		"readable": false
+	}
+]
