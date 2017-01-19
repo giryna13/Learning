700a1ad230fd9607545d6329bd558441348ac776
giryna13 <zhipster@gmail.com>
2017-01-19 13:11:59 +0200

    added some changes

--- javascript/js/Roll and Die.js
+++ javascript/js/Roll and Die.js
@@ -1,6 +1,18 @@
-var firstDie = Math.ceil(6 * Math.random());
-var secondDie = Math.ceil(6 * Math.random());
+var firstDie = Math.ceil(6 * Math.random());//не дуже добре дублювати операції
+var secondDie = Math.ceil(6 * Math.random());//не дуже добре дублювати операції
 
+console.log(firstDie);
+console.log(secondDie);
+console.log(firstDie + secondDie);
+
+//замість дублювання рядків краще написати так:
+
+var getDieRoll = function() {
+  return Math.ceil(6 * Math.random());
+};
+
+var firstDie = getDieRoll();
+var secondDie = getDieRoll();
 console.log(firstDie);
 console.log(secondDie);
 console.log(firstDie + secondDie);
